b2367cc5cce50396577979aacc5097b8d05f7c47
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 22:39:42 +0530

    update the value for the wifiState field

--- app/src/main/java/com/example/wifi_imagebutton_app_java/MainActivity.java
+++ app/src/main/java/com/example/wifi_imagebutton_app_java/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (!wifiState) {
             wifiManager.setWifiEnabled(true);
-
+            wifiState = true;
         }
     }
 }
